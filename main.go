c7bd6835a1d4b7e037f6cd1ce1c42337cff31327
Brice <brice@briceamen.com>
2023-06-04 17:34:14 +0200

    Implement background worker to continuously refresh power data

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bnema/gobatterywatcher/db"
 	"github.com/bnema/gobatterywatcher/handlers"
@@ -9,12 +10,6 @@ import (
 )
 
 func main() {
-	// Read power data from powertop
-	data, err := utils.ReadPowerTop()
-	if err != nil {
-		panic(err)
-	}
-
 	// Create db and tables
 	database, err := db.CreateDB()
 	if err != nil {
@@ -27,17 +22,31 @@ func main() {
 		panic(err)
 	}
 
-	// Process power data
-	uniquePowerData := handlers.ProcessData(data)
+	// Start a goroutine to continuously refresh the power data
+	go func() {
+		for {
+			// Read power data from powertop
+			data, err := utils.ReadPowerTop()
+			if err != nil {
+				panic(err)
+			}
 
-	// Print unique device names
-	for name := range uniquePowerData {
-		fmt.Println(name)
-	}
+			// Process power data
+			uniquePowerData := handlers.ProcessData(data)
 
-	// Insert power data into the database
-	err = handlers.InsertData(database, uniquePowerData)
-	if err != nil {
-		panic(err)
-	}
+			// Insert power data into the database
+			err = handlers.InsertData(database, uniquePowerData)
+			if err != nil {
+				panic(err)
+			}
+
+			// Sleep for 5 seconds
+			time.Sleep(5 * time.Second)
+			fmt.Println("Refreshed power data")
+
+		}
+	}()
+
+	// Keep the main function running forever
+	select {}
 }
